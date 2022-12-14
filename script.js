356ba65db15054c82f97a8fa13ffade5029ae97e
PavelTRN <pashatrn@gmail.com>
2022-12-14 23:12:50 +0100

    Add message object

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 const randomNum = (num) => {
     return Math.floor(Math.random() * num);
 };
+
+// Message object
+const message = {
+    day : ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+    mood : ['good', 'funny', 'bad', 'evil', 'neutral', 'calm', 'concerned'],
+    advice : ['to drink coffe', 'eat', 'relax', 'to see a movie', 'take a walk in the park', 'talk to a friend', 'read to book']
+};
