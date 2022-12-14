b5e331d46ccc8d48c10bf9f32643731062166f3e
PavelTRN <pashatrn@gmail.com>
2022-12-14 23:33:31 +0100

    Output random message

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ const message = {
     mood : ['good', 'funny', 'bad', 'evil', 'neutral', 'calm', 'concerned'],
     advice : ['to drink coffe', 'eat', 'relax', 'to see a movie', 'take a walk in the park', 'talk to a friend', 'read to book']
 };
+
+const randomMessage = () => {
+    let randomDay = randomNum(7);
+    let randomMood = randomNum(7);
+    let randomAdvice = randomNum(7);
+    return  `It's ${message.day[randomDay]} and you're in a ${message.mood[randomMood]} mood? Then I advise you to ${message.advice[randomAdvice]}`;
+};
+console.log(randomMessage());
